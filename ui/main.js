aae8b7ab045fd36744a08caa94a073ff36cb0d37
ktssaiteja <ktssaiteja@gmail.com>
2017-08-17 21:26:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,7 @@
 var button = document.getElementById('btn');
-var counter = 0;
-button.onclick =function () {
-    
-    counter = counter + 1;
+var num = 0;
+button.onclick = function () {
+    num = num + 1;
     var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-};
+    span.innerHTML = num.toString();
+}
