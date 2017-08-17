1d1fe92f9562ecaab4376a854e67f1f54d85cb95
ktssaiteja <ktssaiteja@gmail.com>
2017-08-17 22:30:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,18 @@
 var button = document.getElementById('btn');
-var num = 0;
 button.onclick = function () {
-    num = num + 1;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+       if(request.readyState===XMLHttpRequest.DONE){
+           if(request.status===200){
+               var num=request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = num.toString();
+           }
+       } 
+    };
     var span = document.getElementById('count');
     span.innerHTML = num.toString();
-}
+    
+    request.open('GET', 'http://ktssaiteja.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
