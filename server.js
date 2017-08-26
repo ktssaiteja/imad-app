55f252023fc19d2f9282f33b5e53bec837b70907
ktssaiteja <ktssaiteja@gmail.com>
2017-08-26 18:43:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(cofig);
+var pool = new Pool(config);
 
 app.get('/test-db', function (req, res) {
   pool.query('Select * FROM "user"', function(err, result) {
