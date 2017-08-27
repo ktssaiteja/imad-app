8b437b2d7746782fd8266555c9e8898b72e4c8ea
ktssaiteja <ktssaiteja@gmail.com>
2017-08-27 10:48:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
