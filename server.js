1ecf59c1450c2c20d2aa9e7dd8d7e9038944988b
ktssaiteja <ktssaiteja@gmail.com>
2017-08-27 08:45:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(morgan('combined'));
 
 function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return ['pbkdf2', '10000', salt, hashed.toString('hex')].join('$');
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
