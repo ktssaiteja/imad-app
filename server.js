86f1cf6dfab24a0f52daa3ab53291c1249f4afac
ktssaiteja <ktssaiteja@gmail.com>
2017-08-27 18:57:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
-//var session = require('express-session');
+var session = require('express-session');
 function createTemplate(data) {
     var title=data.title;
     var content=data.content;
@@ -31,10 +31,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-/*app.use(session({
+app.use(session({
     secret: 'someRandomSecretValue',
-    cookie: { maxAge:1000*60}
-}));*/
+    cookie: { maxAge:1000*60*60*24*30}
+}));
 
 function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
@@ -80,7 +80,7 @@ app.post('/login', function(req, res) {
               var hashedPassword = hash(password, salt);
               if(hashedPassword===dbString)
               {
-                  //req.session.auth = {userId: result.rows[0].id};
+                  req.session.auth = {userId: result.rows[0].id};
                   res.send("You have logged in Successfully!");
               }
               else
@@ -94,12 +94,15 @@ app.post('/login', function(req, res) {
     });
 });
 
-/*app.get('/check-login', function (req, res) {
-    if(req.session&&req.session.auth&&req.session.auth.userId)
-    res.send('You are logged in : ' + req.session.auth.userId.toString());
-    else
+app.get('/check-login', function (req, res) {
+    if( req.session && req.session.auth && req.session.auth.userId)
+    {
+        res.send('You are logged in : ' + req.session.auth.userId.toString());
+    } else
+    {
     res.send('You are not logged in.');
-});*/
+    }
+});
 app.get('/hash/:input', function (req, res) {
     var hashedString = hash(req.params.input, 'this_is_a_salt');
     res.send(hashedString);
