46a608f1678a7e34ffb124618e45bafe07444cbf
ktssaiteja <ktssaiteja@gmail.com>
2017-08-27 10:41:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
-
+var bodyParser = require('body-parser');
 
 function createTemplate(data) {
     var title=data.title;
@@ -39,6 +39,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.post('/create-user', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt=crypto.randomBytes(128).toString('hex');
+    var dbString=hash(password, salt);
+    pool.query('INSERT INTO user_credentials (username, password) VALUES ($1, $2)', [username, dbString], function(err, result) {
+        if(err)
+      {
+          res.status(500).send(err.toString());
+      } else
+      {
+          res.send('User successfully created : ' + username);
+      }
+        
+    })
+});
 app.get('/hash/:input', function (req, res) {
     var hashedString = hash(req.params.input, 'this_is_a_salt');
     res.send(hashedString);
